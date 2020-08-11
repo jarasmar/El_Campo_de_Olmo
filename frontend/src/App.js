f1100b9d24d8640e67587840975c3915cf9125d6
Jara Santamaria <jarasmar@gmail.com>
2020-08-11 19:46:13 +0200

    Create components for home and product screen

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import data from './data';
+import { BrowserRouter, Route } from 'react-router-dom';
 import './App.css';
+import HomeScreen from './Screens/HomeScreen';
+import ProductScreen from './Screens/ProductScreen';
 
 function App() {
   
@@ -17,7 +20,7 @@ function App() {
       <div className='grid-container'>
         <header className='header'>
           <div className='brand'>
-            <button onClick={openMenu}> &#9776; </button>
+            <button onClick={ openMenu }> &#9776; </button>
             <a href='index.html'> El Campo de Olmo </a>
           </div>
           <div className='header-links'>
@@ -28,7 +31,7 @@ function App() {
 
         <aside className='sidebar'>
           <h3> CATEGORIAS </h3>
-          <button className='sidebar-close-button' onClick={closeMenu}> x </button>
+          <button className='sidebar-close-button' onClick={ closeMenu }> x </button>
           <ul>
             <li>
               <a href='index.html'> CESTAS </a>
@@ -50,19 +53,19 @@ function App() {
 
         <main className='main'>
           <div className='content'>
-            <Route path='/' exact={true} component={homeScreen} />
-            <Route path='/products/:id' component={ProductScreen} />
-            
+            <Route path='/' exact={true} component={ HomeScreen } />
+            <Route path='/product/:id' component={ ProductScreen } />
+
             <ul className='products'>
               { data.products.map(product => 
                 <li>
                   <div className='product'>
-                    <img className='product-image' src={product.image} alt='product' />
+                    <img className='product-image' src={ product.image } alt='product' />
                     <div className='product-name'> 
-                      <a href='product.html'> {product.name} </a>
+                      <a href='product.html'> { product.name } </a>
                     </div>
-                    <div className='product-rating'> {product.rating} Stars ({product.numReviews} Reviews) </div>
-                    <div className='product-price'> {product.price}€ </div>
+                    <div className='product-rating'> { product.rating } Stars ({ product.numReviews } Reviews) </div>
+                    <div className='product-price'> { product.price }€ </div>
                   </div>
                 </li>
               )}

5345805ac5f6eb2801fff78c0206c7b28ec6e8ff
Jara Santamaria <jarasmar@gmail.com>
2020-08-11 20:10:10 +0200

    Move the code from app.js to its component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -55,21 +55,6 @@ function App() {
           <div className='content'>
             <Route path='/' exact={true} component={ HomeScreen } />
             <Route path='/product/:id' component={ ProductScreen } />
-
-            <ul className='products'>
-              { data.products.map(product => 
-                <li>
-                  <div className='product'>
-                    <img className='product-image' src={ product.image } alt='product' />
-                    <div className='product-name'> 
-                      <a href='product.html'> { product.name } </a>
-                    </div>
-                    <div className='product-rating'> { product.rating } Stars ({ product.numReviews } Reviews) </div>
-                    <div className='product-price'> { product.price }€ </div>
-                  </div>
-                </li>
-              )}
-            </ul>
           </div>
         </main>
 

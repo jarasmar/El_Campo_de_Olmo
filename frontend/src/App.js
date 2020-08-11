5fd6240f6f420c2a86b3ea0b024c98db3e748d89
Jara Santamaria <jarasmar@gmail.com>
2020-08-11 19:14:36 +0200

    Add react-router-dom and routes for main page and products page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -13,63 +13,68 @@ function App() {
   }
 
   return (
-    <div className='grid-container'>
-      <header className='header'>
-        <div className='brand'>
-          <button onClick={openMenu}> &#9776; </button>
-          <a href='index.html'> El Campo de Olmo </a>
-        </div>
-        <div className='header-links'>
-          <a href='signin.html'> USUARIO </a>
-          <a href='cart.html'> CESTA </a>
-        </div>
-      </header>
+    <BrowserRouter>
+      <div className='grid-container'>
+        <header className='header'>
+          <div className='brand'>
+            <button onClick={openMenu}> &#9776; </button>
+            <a href='index.html'> El Campo de Olmo </a>
+          </div>
+          <div className='header-links'>
+            <a href='signin.html'> USUARIO </a>
+            <a href='cart.html'> CESTA </a>
+          </div>
+        </header>
 
-      <aside className='sidebar'>
-        <h3> CATEGORIAS </h3>
-        <button className='sidebar-close-button' onClick={closeMenu}> x </button>
-        <ul>
-          <li>
-            <a href='index.html'> CESTAS </a>
-          </li>
-          <li>
-            <a href='index.html'> FRUTAS </a>
-          </li>
-          <li>
-            <a href='index.html'> HORTALIZAS </a>
-          </li>
-          <li>
-            <a href='index.html'> LEGUMBRES </a>
-          </li>
-          <li>
-            <a href='index.html'> PROCESADOS </a>
-          </li>
-        </ul>
-      </aside>
-
-      <main className='main'>
-        <div className='content'>
-          <ul className='products'>
-            { data.products.map(product => 
-              <li>
-                <div className='product'>
-                  <img className='product-image' src={product.image} alt='product' />
-                  <div className='product-name'> 
-                    <a href='product.html'> {product.name} </a>
-                  </div>
-                  <div className='product-rating'> {product.rating} Stars ({product.numReviews} Reviews) </div>
-                  <div className='product-price'> {product.price}€ </div>
-                </div>
-              </li>
-            )}
+        <aside className='sidebar'>
+          <h3> CATEGORIAS </h3>
+          <button className='sidebar-close-button' onClick={closeMenu}> x </button>
+          <ul>
+            <li>
+              <a href='index.html'> CESTAS </a>
+            </li>
+            <li>
+              <a href='index.html'> FRUTAS </a>
+            </li>
+            <li>
+              <a href='index.html'> HORTALIZAS </a>
+            </li>
+            <li>
+              <a href='index.html'> LEGUMBRES </a>
+            </li>
+            <li>
+              <a href='index.html'> PROCESADOS </a>
+            </li>
           </ul>
-        </div>
-      </main>
+        </aside>
 
-      <footer className='footer'>
-        Copyright © 2020 El Campo de Olmo. Diseñado por Jara S.
-      </footer>
-    </div>
+        <main className='main'>
+          <div className='content'>
+            <Route path='/' exact={true} component={homeScreen} />
+            <Route path='/products/:id' component={ProductScreen} />
+            
+            <ul className='products'>
+              { data.products.map(product => 
+                <li>
+                  <div className='product'>
+                    <img className='product-image' src={product.image} alt='product' />
+                    <div className='product-name'> 
+                      <a href='product.html'> {product.name} </a>
+                    </div>
+                    <div className='product-rating'> {product.rating} Stars ({product.numReviews} Reviews) </div>
+                    <div className='product-price'> {product.price}€ </div>
+                  </div>
+                </li>
+              )}
+            </ul>
+          </div>
+        </main>
+
+        <footer className='footer'>
+          Copyright © 2020 El Campo de Olmo. Diseñado por Jara S.
+        </footer>
+      </div>
+    </BrowserRouter>
   );
 }
 
